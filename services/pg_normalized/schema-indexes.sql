4e2f34900ebecc975d71ea1f54703c83ef637662
CurtisSalinger <curtisjacksalinger@gmail.com>
2021-04-24 21:43:09 -0700

    test2

--- services/pg_normalized/schema-indexes.sql
+++ services/pg_normalized/schema-indexes.sql
@@ -2,4 +2,4 @@ CREATE INDEX idx_tweet_tags_id_tweets_tag_lang ON tweet_tags (id_tweets,tag,lang
 
 CREATE INDEX idx_tweets_id_tweets ON tweets (id_tweets);
 
-CREATE INDEX idx_tweets_text ON tweets gin(text);
+CREATE INDEX idx_tweets_fts ON tweets USING gin(to_tsvector('english',text);
