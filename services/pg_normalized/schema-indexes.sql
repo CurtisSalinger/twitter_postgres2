cc09ddb8389302f67faa6051800ea076d136fb37
CurtisSalinger <curtisjacksalinger@gmail.com>
2021-04-26 17:28:37 -0700

    all passing

--- services/pg_normalized/schema-indexes.sql
+++ services/pg_normalized/schema-indexes.sql
@@ -1,5 +1,5 @@
-CREATE INDEX idx_tweet_tags_id_tweets_tag_lang ON tweet_tags (id_tweets,tag,lang);
+CREATE INDEX idx_tweet_tags_id_tweets_tag_lang ON tweet_tags (id_tweets,tag);
 
-CREATE INDEX idx_tweets_id_tweets ON tweets (id_tweets);
+CREATE INDEX idx_tweets_id_tweets ON tweets (id_tweets,lang);
 
-CREATE INDEX idx_tweets_fts ON tweets USING gin(to_tsvector('english',text);
+CREATE INDEX idx_tweets_fts ON tweets USING gin(to_tsvector('english',text));
--- sql.denormalized/02.sql
+++ sql.denormalized/02.sql
@@ -1,3 +1,23 @@
 /*
  * Calculates the hashtags that are commonly used with the hashtag #coronavirus
  */
+
+SELECT '#' || tags AS tag,
+COUNT(distinct id) FROM
+      (
+	SELECT
+	jsonb_array_elements(COALESCE((((data->'extended_tweet'::text) -> 'entities'::
+	text) -> 'hashtags'::text), ((data->'entities'::text) -> 'hashtags'::text)))->>'text' AS tags,
+	data->'id' AS id
+	FROM tweets_jsonb
+	WHERE
+	(data->'extended_tweet'::text -> 'entities'::text -> 'hashtags'::text) @>
+	'[{"text":"coronavirus"}]' OR
+	(data->'entities'::text -> 'hashtags'::text) @> '[{"text":"coronavirus"}]'
+      ) AS filler
+GROUP BY (1)
+ORDER BY count DESC, tag
+limit 1000;
+
+--Arrows Matched
+--Line 11 need 'text'?
